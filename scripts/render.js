ba494846f1dcc04b22cfb8e8764e1381098018f6
Gaspar <gasparlarrainv@gmail.com>
2026-04-06 19:05:28 -0400

    Change Size

--- scripts/render.js
+++ scripts/render.js
@@ -158,7 +158,7 @@ function buildSVG(item, typeConfig) {
     STARTING: '#ffffff',
   };
 
-  return `<svg xmlns="http://www.w3.org/2000/svg" width="400" height="250" viewBox="0 0 400 200" fill="none">
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="250" viewBox="0 0 400 200" fill="none">
 <style>
   text { font-family: Verdana, sans-serif; }
   .t   { font-weight: bold; }
